d2e253e0582306eb26b163fface890e743331c18
alex0730 <wuyingya0730@126.com>
2021-01-29 13:58:56 +0800

    修改 后台管理系统基础架构，去除无用代码

--- src/main/java/com/lee/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/lee/service/impl/ArticleServiceImpl.java
@@ -6,20 +6,17 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
 import com.lee.entity.Article;
-import com.lee.entity.Comment;
 import com.lee.mapper.ArticleMapper;
 import com.lee.mapper.CommentMapper;
 import com.lee.service.ArticleService;
-import com.lee.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Map;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author lee
@@ -32,39 +29,41 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> impl
     @Autowired
     CommentMapper commentMapper;
 
-    public IPage<Map<String,Object>> getPageInfo(Map<String,Object> queryParam,int offset,int size){
+    public IPage<Map<String, Object>> getPageInfo(Map<String, Object> queryParam, int offset, int size) {
         QueryWrapper<Article> queryWrapper = new QueryWrapper<>();
-        if(queryParam.get("title") != null) {
-            queryWrapper.like("title",queryParam.get("title"));
+        if (queryParam.get("title") != null) {
+            queryWrapper.like("title", queryParam.get("title"));
         }
         queryWrapper.orderByDesc("create_time");
-        Page<Article> page = new Page<Article>(offset/size+1,size);
-        IPage<Map<String,Object>> mapIPage = articleMapper.selectMapsPage(page,queryWrapper);
-        return mapIPage;
-    }
-    public IPage<Map<String,Object>> getPageInfoByPageNum(Map<String,Object> queryParam,int pagenum,int size){
-        QueryWrapper<Article> queryWrapper = new QueryWrapper<>();
-        if(queryParam.get("title") != null) {
-            queryWrapper.like("title",queryParam.get("title"));
-        }
-        if(queryParam.get("cateid") != null) {
-            queryWrapper.eq("cate_id",queryParam.get("cateid"));
-        }
-        queryWrapper.orderByDesc("create_time");
-        Page<Article> page = new Page<Article>(pagenum,size);
-        IPage<Map<String,Object>> mapIPage = articleMapper.selectMapsPage(page,queryWrapper);
+        Page<Article> page = new Page<Article>(offset / size + 1, size);
+        IPage<Map<String, Object>> mapIPage = articleMapper.selectMapsPage(page, queryWrapper);
         return mapIPage;
     }
 
-    public boolean deleteArticleAndCommentById(Integer id){
+    public IPage<Map<String, Object>> getPageInfoByPageNum(Map<String, Object> queryParam, int pagenum, int size) {
+        QueryWrapper<Article> queryWrapper = new QueryWrapper<>();
+        if (queryParam.get("title") != null) {
+            queryWrapper.like("title", queryParam.get("title"));
+        }
+        if (queryParam.get("cateid") != null) {
+            queryWrapper.eq("cate_id", queryParam.get("cateid"));
+        }
+        queryWrapper.orderByDesc("create_time");
+        Page<Article> page = new Page<Article>(pagenum, size);
+        IPage<Map<String, Object>> mapIPage = articleMapper.selectMapsPage(page, queryWrapper);
+        return mapIPage;
+    }
+
+    public boolean deleteArticleAndCommentById(Integer id) {
         commentMapper.deleteByArticleId(id);
         return SqlHelper.delBool(articleMapper.deleteById(id));
     }
 
-    public Article getNextInfo(Map<String,Object> searchParam){
+    public Article getNextInfo(Map<String, Object> searchParam) {
         return this.articleMapper.getNextInfo(searchParam);
     }
-    public Article getPreInfo(Map<String,Object> searchParam){
+
+    public Article getPreInfo(Map<String, Object> searchParam) {
         return this.articleMapper.getPreInfo(searchParam);
     }
 }
